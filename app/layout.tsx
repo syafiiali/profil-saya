99c4c6aae1e10466e15035c59f036925e0e0afda
syafiiali <aliisyafii@gmail.com>
2026-08-20 21:59:02 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,36 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// Bagian ini yang diubah:
+// Metadata lengkap dengan fitur Open Graph (Pratinjau Gambar & Deskripsi)
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.alisyafii.id"),
   title: "Ahmad Ali Syafi'i",
-  description: "dosenxplore",
+  description: "Dosen & Peneliti Teknik Pertambangan, Teknologi Geospasial, dan Pengabdian Masyarakat.",
+  icons: {
+    icon: "/icon.svg",
+  },
+  openGraph: {
+    title: "Ahmad Ali Syafi'i",
+    description: "Dosen & Peneliti Teknik Pertambangan, Teknologi Geospasial, dan Pengabdian Masyarakat.",
+    url: "https://www.alisyafii.id",
+    siteName: "Ahmad Ali Syafi'i",
+    images: [
+      {
+        url: "/opengraph-image.png", // Mengambil gambar dari folder public/batu-mulia.jpg
+        width: 1200,
+        height: 630,
+        alt: "Ahmad Ali Syafi'i - Featured Image",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ahmad Ali Syafi'i",
+    description: "Dosen & Peneliti Teknik Pertambangan, Teknologi Geospasial, dan Pengabdian Masyarakat.",
+    images: ["/opengraph-image.png"], // 
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
